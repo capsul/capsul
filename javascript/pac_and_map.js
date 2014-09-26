f0c3998cdfbae8dee278b5c0e188907e760aeecf
Gannon Curran <gannoncurran@mac.com>
2014-09-25 22:45:49 -0700

    initial custom map pins

--- javascript/pac_and_map.js
+++ javascript/pac_and_map.js
@@ -1,6 +1,6 @@
 var capsulMap = (function() {
 
-	var map, pacInput, mapContainer
+	var mapContainer, map, pacInput, pac, pinLocations = []
 
   var mapOptions = {
 	  mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -15,22 +15,65 @@ var capsulMap = (function() {
 	  streetViewControl: false
   }
 
-	var pinLocations = []
-
 	var granuleMarker = {
-		url: "../images/dot.png",
+		url: "images/dot.png",
 	  size: new google.maps.Size(10, 10),
 	  origin: new google.maps.Point(0,0),
 	  anchor: new google.maps.Point(5, 5)
   }
 
+  var updateMapPlaces = function() {
+
+      var places = pac.getPlaces();
+
+      if (places.length === 0) {
+
+        return;
+
+      } else if (places.length === 1) {
+
+      	var place = places[0];
+      	var marker = new google.maps.Marker({
+      		map: map,
+      		title: place.name,
+      		position: place.geometry.location
+      	});
+
+      	var bounds = place.geometry.viewport;
+
+      } else {
+
+	      // For each place, get the icon, place name, and location.
+	      var bounds = new google.maps.LatLngBounds();
+	      for (var i = 0, place; place = places[i]; i++) {
+
+	        // Create a marker for each place.
+	        var marker = new google.maps.Marker({
+	          map: map,
+	          position: place.geometry.location,
+	        	icon: granuleMarker,
+	          title: place.name
+	        });
+
+	        // markers.push(marker);
+
+	        bounds.extend(place.geometry.location);
+	      }
+	    }
+
+      map.fitBounds(bounds);
+
+    }
+
   return {
 
   	initialize: function() {
-  		pacInput = $("#pac-input")[0]
   		mapContainer = $("#map-canvas")[0]
 		  map = new google.maps.Map(mapContainer, mapOptions)
-  	}, 
+  		pacInput = $("#pac-input")[0]
+  		pac = new google.maps.places.SearchBox(pacInput)
+  		google.maps.event.addListener(pac, 'places_changed', updateMapPlaces)
+  	},
 
   	setPins: function(locations) {},
 
